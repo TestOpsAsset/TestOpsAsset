cf1dcd182ae6e9ac88b08aa79ed3fb74ae0062c5
TestOpsAsset <testopsasset@gmail.com>
2016-07-07 10:04:37 -0500

    Update data.java

--- src/com/frontEndApp_Scripts/data.java
+++ src/com/frontEndApp_Scripts/data.java
@@ -9,10 +9,9 @@ public class data {
 	public static final String new_FName= "testOpsName";
 	public static final String new_LName = "testingCap";
 	public static final String skytap ="https://cloud.skytap.com/vms/e7a95d9060731b51368a061d05086569/desktops?sort=name&thumbnails=shown&view=html5";
-		
 	public static final String Chrome1="webdriver.chrome.driver";
 	public static final String Chrome2=	"C://Users/l.enriquez.rodriguez/Documents/Eclipse Projects/chromedriver.exe";
-
+	//Internet Explorer Driver setting
 	public static final String IE1="webdriver.ie.driver";
 	public static final String IE2="G://Trainings//Eclipse projects//IEDriverServer.exe";
 			
